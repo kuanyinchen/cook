57871721ae7a497b862c9172cb81c355dae63c63
Ingrid Chen <ingrid831206@gmail.com>
2020-12-14 15:41:24 +0800

    test

--- server/routes/formtest.js
+++ server/routes/formtest.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
 const { query } = require('../models/mysqlcon');
+const { uploadS3 } = require('../models/multer_model');
+const cpUpload = uploadS3.fields([{ name: 'cover', maxCount: 1 }]);
 
-router.post('/formtest', (req, res) => {
+router.post('/formtest', cpUpload, async (req, res) => {
     const body = req.body;
     console.log(body);
+    const image = req.files.cover[0].key;
 
     info = {
         name: body.name,
         password: body.password,
+        image: image,
     };
 
     statement = 'INSERT INTO test SET ?';
